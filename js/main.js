5a2f8c52bd697fbfc6adfcb8f8e574f801121a04
andela-fokosun <okosunuzflorence@gmail.com>
2016-10-23 20:39:39 +0100

    add more html tags

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var transition = {
     quotes: [
         'Welcome to Andela',
-        'Welcome to Digital Bootcamp', 
-        'Be the first to know how to do this', 
-        'take your time to read this', 
+        'Welcome to Digital Bootcamp',
+        'Be the first to know how to do this',
+        'take your time to read this',
         'fifth heroku'
      ],
     transition_index: 0,
@@ -46,7 +46,42 @@ var transition = {
     }
 };
 
+var portfolio = {
+  samuel_james: {
+    gender: 'male',
+    occupation: 'programmer',
+    pet: 'cats'
+  },
+
+  florence_okosun: {
+    gender: 'female',
+    occupation: 'programmer',
+    pet: 'dogs'
+  },
+
+  olaide_ojewale: {
+    gender: 'male',
+    occupation: 'programmer',
+    pet: 'none'
+  },
+
+  init: function() {
+    $('#samueljamesgender').text(portfolio.samuel_james.gender);
+    $('#samueljamesoccupation').text(portfolio.samuel_james.occupation);
+    $('#samueljamespets').text(portfolio.samuel_james.pet);
+
+    $('#florenceokosungender').text(portfolio.florence_okosun.gender);
+    $('#florenceokosunoccupation').text(portfolio.florence_okosun.occupation);
+    $('#florenceokosunpets').text(portfolio.florence_okosun.pet);
+
+    $('#olaideojewalegender').text(portfolio.olaide_ojewale.gender);
+    $('#olaideojewaleoccupation').text(portfolio.olaide_ojewale.occupation);
+    $('#olaideojewalepets').text(portfolio.olaide_ojewale.pet);
+  },
+
+};
+
 window.onload = function() {
     transition.init();
+    portfolio.init();
 }
-
